1fe49c486a3fc082af89971e491daef278ccccb0
hoanthieuhuyen <hoanthieuhuyen@gmail.com>
2017-08-15 23:43:43 +0700

    add new project

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { appRouters } from './app.routers';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRouters)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
