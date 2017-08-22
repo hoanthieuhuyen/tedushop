da26c375888de68410792c1452c99950269693de
hoanthieuhuyen <hoanthieuhuyen@gmail.com>
2017-08-23 00:43:58 +0700

    https://github.com/hoanthieuhuyen/tedushop-angular2.git

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,22 +1,51 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import { Router } from '@angular/router';
+import { SystemConstants } from './../common/system.constants';
+import { AuthenService } from './authen.service';
+import { Observable } from 'rxjs/Observable';
+import { NotificationService } from './notification.service';
+import { UtilityService } from './utility.service';
+import {MessageConstants} from './../common/message.constants';
 
 @Injectable()
 export class DataService {
-
-  constructor() { }
+  private headers: Headers;
+  constructor(private _http: Http, private _router: Router, private _authenService: AuthenService,private _notificationService:NotificationService,private _utilityService:UtilityService) { }
   get(uri: string) {
-    return null;
+    this.headers.delete("Authorization");
+    this.headers.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().assess_token);
+    return this._http.get(SystemConstants.BASE_API + uri, { headers: this.headers }).map(this.extractData);
   }
   post(uri: string, data?: any) {
-    return null;
+    this.headers.delete("Authorization");
+    this.headers.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().assess_token);
+    return this._http.post(SystemConstants.BASE_API + uri, data, { headers: this.headers }).map(this.extractData);
   }
   put(uri: string, data?: any) {
-    return null;
+    this.headers.delete("Authorization");
+    this.headers.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().assess_token);
+    return this._http.put(SystemConstants.BASE_API + uri, data, { headers: this.headers }).map(this.extractData);
   }
   delete(uri: string, key: string, id: string) {
-    return null;
+    this.headers.delete("Authorization");
+    this.headers.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().assess_token);
+    return this._http.delete(SystemConstants.BASE_API + uri + "/?" + key + "=" + id, { headers: this.headers }).map(this.extractData);
   }
   postFile(uri: string, data?: any) {
-
+    let postFileHeader = new Headers();
+    postFileHeader.delete("Authorization");
+    postFileHeader.append("Authorization", "Bearer" + this._authenService.getLoggedInUser().assess_token);
+    return this._http.post(SystemConstants.BASE_API + uri, data, { headers: this.headers }).map(this.extractData);
+  }
+  private extractData(res: Response) {
+    let body = res.json();
+    return body || {};
+  }
+  public handleError(error: any) {
+    if (error.status == 401) {
+      localStorage.removeItem(SystemConstants.CURRENT_USER);
+      this._no
+    }
   }
 }
