31e041d4ac2f90673a31bc6e4b1ee4664a836beb
hoanthieuhuyen <hoanthieuhuyen@gmail.com>
2017-08-28 23:34:31 +0700

    https://github.com/hoanthieuhuyen/tedushop-angular2.git

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { FormsModule } from '@angular/forms';
+import { NotificationService } from '../core/services/notification.service';
+import { AuthenService } from '../core/services/authen.service';
 export const routes: Routes = [
   { path: '', component: LoginComponent }
 ]
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(routes)
   ],
+  providers: [NotificationService, AuthenService],
   declarations: [LoginComponent]
 })
 export class LoginModule { }
